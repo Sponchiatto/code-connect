0707ae73f07a12f1bb8b93e2bfad71891327e243
Sponchiatto <rafael_sponchiatto@hotmail.com>
2025-01-11 17:44:30 -0300

    Finalizando uma aplicação introdutória a next.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import { CardPost } from "@/components/CardPost";
 import logger from "@/logger";
 import styles from "./page.module.css";
+import Link from "next/link";
 async function getAllPosts(page) {
   try {
     const response = await fetch(
       `http://localhost:3042/posts?_page=${page}&_per_page=6`
     );
     if (!response.ok) throw new Error("Falha na rede");
+    logger.info("Posts recebidos com sucesso");
     return response.json();
   } catch (error) {
     logger.error("Ops, algo correu mal: " + error.message);
@@ -14,13 +16,18 @@ async function getAllPosts(page) {
   }
 }
 
-export default async function Home() {
-  const { data: posts } = await getAllPosts(1);
+export default async function Home({ searchParams }) {
+  const currentpage = searchParams?.page || 1;
+  const { data: posts, prev, next } = await getAllPosts(currentpage);
   return (
     <main className={styles.grid}>
       {posts.map((post) => (
         <CardPost key={post.id} post={post} />
       ))}
+      <div className={styles.links}>
+        {prev && <Link href={`/?page=${prev}`}>Página anterior</Link>}
+        {next && <Link href={`/?page=${next}`}>Próxima página</Link>}
+      </div>
     </main>
   );
 }
